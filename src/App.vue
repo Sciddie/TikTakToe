5ff3a712a8ded0a17d1ad2c6e45462acebf53253
Florian <codecentricprakti@gmail.com>
2023-02-28 17:49:28 +0100

    15Puzzle

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,11 @@
   function initialState() {
     let gamegridRender = []
     for (let i = 0; i<16;i++) {
-      gamegridRender.push({label: i+1, imagePosition: String(Math.floor(i/4)*100) + "px " + String((i*100)%400) + "px"})
+      gamegridRender.push({label: i+1, imagePosition: "-" + String((i*100)%400) + "px " + "-" + String(Math.floor(i/4)*100) + "px"})
     }
     console.log(gamegridRender)
     return {
-      gamegrid: gamegridRender.shuffle(),
+      gamegrid: gamegridRender,
       running: true,
       winningText: "Das Spiel läuft noch!"
     }
@@ -34,8 +34,8 @@
         if (this.gamegrid[field].label === 16) return
         let emptyIndex = this.gamegrid.findIndex(e => e.label === 16)
         if (!(Math.abs(emptyIndex - field) === 1 || Math.abs(emptyIndex - field) === 4)) return
-        this.gamegrid[emptyIndex]['label'] = this.gamegrid[field].label
-        this.gamegrid[field]['label'] = 16
+        this.gamegrid[emptyIndex] = this.gamegrid[field]
+        this.gamegrid[field] = {label: 16, imagePosition: "300px 300px"}
         if (this.hasWon()) {
           this.running = false
           this.winningText = "Du hast gewonnen!"
@@ -61,7 +61,7 @@
   <p class="winning-text">{{winningText}}</p>
   <div class="gamegrid">
     <div v-for="field in 16" class="inline-block">
-      <button :id="'field' + String(field-1)" class="gamegrid-button" :class="{'correct-button' : isNumberCorrect(field), 'empty-button' : gamegrid[field-1].label===16}" :style="{'background-image': isEmpty(field-1) ? 'none' : 'url(\'https://cdn2.thecatapi.com/images/3tv.jpg\')'}" @click="fieldPressed(field-1)" >{{gamegrid[field-1].label}}</button>
+      <button class="gamegrid-button" :class="{'correct-button' : isNumberCorrect(field), 'empty-button' : gamegrid[field-1].label===16}" :style="{'background-image': isEmpty(field-1) ? 'none' : 'url(\'https://cdn2.thecatapi.com/images/3tv.jpg\')', 'background-position':  this.gamegrid[field-1].imagePosition}" @click="fieldPressed(field-1)" >{{gamegrid[field-1].label}}</button>
       <br v-if="field % 4 === 0"/>
     </div>
   </div>
@@ -98,55 +98,6 @@
     font-family: Arial;
     font-weight: bold;
     font-size: 35px;
-  }
-
-  #field0 {
-    background-position: v-bind(gamegrid[0][imagePosition]);
-  }
-  #field1 {
-    background-position: v-bind(gamegrid[1][imagePosition]);
-  }
-  #field2 {
-    background-position: v-bind("gamegrid[2][imagePosition]");
-  }
-  #field3 {
-    background-position: v-bind("gamegrid[3][imagePosition]");
-  }
-  #field4 {
-    background-position: v-bind(gamegrid[4][imagePosition]);
-  }
-  #field5 {
-    background-position: v-bind(gamegrid[5][imagePosition]);
-  }
-  #field6 {
-    background-position: v-bind(gamegrid[6][imagePosition]);
-  }
-  #field7 {
-    background-position: v-bind(gamegrid[7][imagePosition]);
-  }
-  #field8 {
-    background-position: v-bind(gamegrid[8][imagePosition]);
-  }
-  #field9 {
-    background-position: v-bind(gamegrid[9][imagePosition]);
-  }
-  #field10 {
-    background-position: v-bind(gamegrid[10][imagePosition]);
-  }
-  #field11 {
-    background-position: v-bind(gamegrid[11][imagePosition]);
-  }
-  #field12 {
-    background-position: v-bind(gamegrid[12][imagePosition]);
-  }
-  #field13 {
-    background-position: v-bind(gamegrid[13][imagePosition]);
-  }
-  #field14 {
-    background-position: v-bind(gamegrid[14][imagePosition]);
-  }
-  #field15 {
-    background-position: v-bind(gamegrid[15][imagePosition]);
   }
 </style>
 <style scoped>
