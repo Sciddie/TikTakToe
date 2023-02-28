f8600238bfd9d6b5d47a41a3482661a2e9a496d7
Florian <codecentricprakti@gmail.com>
2023-02-28 16:36:39 +0100

    15Puzzle

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script>
-
   Object.defineProperty(Array.prototype, 'shuffle', {
     value: function() {
       for (let i = this.length - 1; i > 0; i--) {
@@ -10,8 +9,13 @@
     }
   });
   function initialState() {
+    let gamegridRender = []
+    for (let i = 0; i<16;i++) {
+      gamegridRender.push({label: i+1, imagePosition: String(Math.floor(i/4)*100) + "px " + String((i*100)%400) + "px"})
+    }
+    console.log(gamegridRender)
     return {
-      gamegrid: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,"_"].shuffle(),
+      gamegrid: gamegridRender.shuffle(),
       running: true,
       winningText: "Das Spiel läuft noch!"
     }
@@ -22,15 +26,16 @@
     },
     methods: {
       isNumberCorrect(field) {
-        return field === this.gamegrid.indexOf(field) +1
+        if (field == 16) {return false}
+        return field === this.gamegrid.findIndex(e => e.label === field) +1
       },
       fieldPressed(field) {
         if (!(this.running)) return
-        if (this.gamegrid[field] === "_") return
-        let emptyIndex = this.gamegrid.indexOf("_")
+        if (this.gamegrid[field].label === 16) return
+        let emptyIndex = this.gamegrid.findIndex(e => e.label === 16)
         if (!(Math.abs(emptyIndex - field) === 1 || Math.abs(emptyIndex - field) === 4)) return
-        this.gamegrid[emptyIndex] = this.gamegrid[field]
-        this.gamegrid[field] = "_"
+        this.gamegrid[emptyIndex]['label'] = this.gamegrid[field].label
+        this.gamegrid[field]['label'] = 16
         if (this.hasWon()) {
           this.running = false
           this.winningText = "Du hast gewonnen!"
@@ -38,9 +43,12 @@
       },
       hasWon() {
         for (let i = 0; i < 16; ++i) {
-          if (this.gamegrid[i] !== [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,"_"][i]) return false;
+          if (this.gamegrid[i].label !== [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16][i]) return false;
         }
         return true;
+      },
+      isEmpty(field) {
+        return this.gamegrid[field].label === 16
       },
       reset() {
         Object.assign(this.$data, initialState())
@@ -53,7 +61,7 @@
   <p class="winning-text">{{winningText}}</p>
   <div class="gamegrid">
     <div v-for="field in 16" class="inline-block">
-      <button class="gamegrid-button" :class="{'correct-button' : isNumberCorrect(field), 'empty-button' : gamegrid[field-1]==='_'}" @click="fieldPressed(field-1)">{{gamegrid[field-1]}}</button>
+      <button :id="'field' + String(field-1)" class="gamegrid-button" :class="{'correct-button' : isNumberCorrect(field), 'empty-button' : gamegrid[field-1].label===16}" :style="{'background-image': isEmpty(field-1) ? 'none' : 'url(\'https://cdn2.thecatapi.com/images/3tv.jpg\')'}" @click="fieldPressed(field-1)" >{{gamegrid[field-1].label}}</button>
       <br v-if="field % 4 === 0"/>
     </div>
   </div>
@@ -90,6 +98,55 @@
     font-family: Arial;
     font-weight: bold;
     font-size: 35px;
+  }
+
+  #field0 {
+    background-position: v-bind(gamegrid[0][imagePosition]);
+  }
+  #field1 {
+    background-position: v-bind(gamegrid[1][imagePosition]);
+  }
+  #field2 {
+    background-position: v-bind("gamegrid[2][imagePosition]");
+  }
+  #field3 {
+    background-position: v-bind("gamegrid[3][imagePosition]");
+  }
+  #field4 {
+    background-position: v-bind(gamegrid[4][imagePosition]);
+  }
+  #field5 {
+    background-position: v-bind(gamegrid[5][imagePosition]);
+  }
+  #field6 {
+    background-position: v-bind(gamegrid[6][imagePosition]);
+  }
+  #field7 {
+    background-position: v-bind(gamegrid[7][imagePosition]);
+  }
+  #field8 {
+    background-position: v-bind(gamegrid[8][imagePosition]);
+  }
+  #field9 {
+    background-position: v-bind(gamegrid[9][imagePosition]);
+  }
+  #field10 {
+    background-position: v-bind(gamegrid[10][imagePosition]);
+  }
+  #field11 {
+    background-position: v-bind(gamegrid[11][imagePosition]);
+  }
+  #field12 {
+    background-position: v-bind(gamegrid[12][imagePosition]);
+  }
+  #field13 {
+    background-position: v-bind(gamegrid[13][imagePosition]);
+  }
+  #field14 {
+    background-position: v-bind(gamegrid[14][imagePosition]);
+  }
+  #field15 {
+    background-position: v-bind(gamegrid[15][imagePosition]);
   }
 </style>
 <style scoped>
